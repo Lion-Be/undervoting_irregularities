70b1260ec00a4cf7be59b33222117755a7ea37a7
Lion Behrens <behrens@uni-mannheim.de>
2022-05-19 15:27:33 +0200

    Update _1 est_fraud.R

--- _1 est_fraud.R
+++ _1 est_fraud.R
@@ -13,49 +13,52 @@ est_fraud <- function(entities,         # vector with eligible voters
   dat_emp <- dat_emp[order(dat_emp$underperc_emp),]
   colnames(dat_emp) <- c("under_perc", "winner_share")
   
-  share_estimate <- rep(NA, n_iter)
+  euc_estimate <- mahalanobis_estimate <-
+    rep(NA, n_iter)
+  
   for (iter in 1:n_iter) {
     
     # simulate data under different share_fraud parameters
-    df_list <- list()
-    euc_dist <- rep(NA, length(seq(0, 0.99,0.01)))
+    euc_dist <- rep(NA, length(seq(0, 0.99,0.02)))
+    mahalanobis_dist <- rep(NA, length(seq(0, 0.99,0.02)))
     id <- 0
-    for (share in seq(0, 0.99,0.01)) {
+    for (share in seq(0, 0.99,0.02)) {
       id <- id+1
-      df_list[[id]] <- gen_data(entities = entities, 
-                                turnout_probs = turnout_probs, 
-                                winner_probs = winner_probs, 
-                                undervoting_n = undervoting_n, 
-                                undervoting_sd = undervoting_sd, 
-                                share_fraud = share)
+      df <- gen_data(entities = entities, 
+                     turnout_probs = turnout_probs, 
+                     winner_probs = winner_probs, 
+                     undervoting_n = undervoting_n, 
+                     undervoting_sd = undervoting_sd, 
+                     share_fraud = share)
       
       # calculate distance metric between p(under_share, winner_share) and 
       # p(underperc_emp, pw_emp) for every data pair 
-      dat_sim <- as.data.frame(cbind(df_list[[id]]$under_perc, df_list[[id]]$winner_share))
+      dat_sim <- as.data.frame(cbind(df$under_perc, df$winner_share))
       colnames(dat_sim) <- c("under_perc", "winner_share")
       dat_sim <- dat_sim[dat_sim$under_perc!=0,]
       dat_sim <- dat_sim[order(dat_sim$under_perc),]
-      #### calculate Euclidean distance between each row in dat_emp and dat_sim, sum up
-      euc_dist_ind <- rep(NA, nrow(dat_sim))
-      if(nrow(dat_emp) != nrow(dat_sim)) {
-        print("nrow(dat_emp) != nrow(dat_sim)")
-        break
-      } # end if
       
-      for (row in 1:nrow(dat_sim)) 
-        euc_dist_ind[row] <- dist(rbind(dat_emp[row,], dat_sim[row,]), method="euclidean")
-      
-      euc_dist[id] <- sum(euc_dist_ind)
+      # calculate Euclidean distance between each row in dat_emp and dat_sim, sum up
+      euc_dist[id] <- sum(diag(rdist(dat_emp, dat_sim)))
+      mahalanobis_dist[id] <- sum(mahalanobis(dat_emp, colMeans(dat_sim), cov(dat_sim)))
     } # end for share
  
     # identify fraud parameter that minimizes distance metric
-    share_estimate[iter] <- seq(0, 0.99,0.01)[which.min(euc_dist)]
+    euc_estimate[iter] <- seq(0, 0.99,0.02)[which.min(euc_dist)]
+    mahalanobis_estimate[iter] <- seq(0, 0.99,0.02)[which.min(mahalanobis_dist)]
+    print(iter)
     
   } # end for iter
   
-   return(c(mean(share_estimate), 
-           quantile(sort(share_estimate), 0.025),
-           quantile(sort(share_estimate), 0.975)
-           ))
-  
+  out_list <- list()
+  out_list[["euclidean"]] <- c(mean(euc_estimate), 
+                               quantile(sort(euc_estimate), 0.025),
+                               quantile(sort(euc_estimate), 0.975)
+                                )
+  out_list[["mahalanobis"]] <- c(mean(mahalanobis_estimate), 
+                                 quantile(sort(mahalanobis_estimate), 0.025),
+                                 quantile(sort(mahalanobis_estimate), 0.975)
+                                 )
+  return(out_list)
+ 
 }
